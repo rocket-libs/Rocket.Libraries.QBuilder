a0a567adb22f06875dac9ebe4193042b78b7fd1b
nyingi <nyingimaina@gmail.com>
2019-10-23 09:12:24 +0300

    made exceptions be thrown for when wherein has an empty list

--- src/Rocket.Libraries.Qurious/Helpers/WhereInFilterMaker.cs
+++ src/Rocket.Libraries.Qurious/Helpers/WhereInFilterMaker.cs
@@ -12,15 +12,18 @@ namespace Rocket.Libraries.Qurious.Helpers
             {
                 throw new Exception("Cannot build a where clause from an null list of values");
             }
+
             if (values.Count == 0)
             {
-                return string.Empty;
+                throw new Exception("No list of values provided. Exception is being thrown, as this condition is ambiguous, and going ahead would likely produce an unpredictable result.");
             }
+
             var args = string.Empty;
             foreach (var value in values)
             {
                 args += $",'{value}'";
             }
+
             args = $"({args.Substring(1)})";
             return args;
         }
