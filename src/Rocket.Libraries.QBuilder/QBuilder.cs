27f7b39025cba951e5f05c89dbab0580f85f6740
nyingi <nyingimaina@gmail.com>
2019-11-12 17:18:06 +0300

    Re-organized project structure

--- src/Rocket.Libraries.QBuilder/QBuilder.cs
+++ src/Rocket.Libraries.QBuilder/QBuilder.cs
@@ -151,19 +151,6 @@
             return suffixedQuery.Trim();
         }
 
-        /* [Obsolete("Clumsy. don't use. Instead use the 'UseDerivedTableJoiner' in the Joiner(s)")]
-         public JoinBuilder FinishJoinToDerivedTable()
-         {
-             var noInnerDescription = InnerSelectDescription == null;
-
-             if (noInnerDescription)
-             {
-                 throw new Exception($"You are not in a '{nameof(JoinBuilder.BeginInnerJoinToDerivedTable)}' section. Did you mean to call '{nameof(JoinBuilder.Then)}' instead?");
-             }
-
-             return InnerSelectDescription.Parent;
-         }*/
-
         private string GetWrappedInSelectAlias(string query)
         {
             var result = $"Select * from ({query}) as {DerivedTableName}";
