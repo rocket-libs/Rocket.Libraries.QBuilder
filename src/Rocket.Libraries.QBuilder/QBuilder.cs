65f8b949fdc19fcf0a796c67ac88163b46a8e12a
Nyingi <nyingi.maina@gmail.com>
2023-05-31 21:35:32 +0300

    Added doc comments

--- src/Rocket.Libraries.QBuilder/QBuilder.cs
+++ src/Rocket.Libraries.QBuilder/QBuilder.cs
@@ -7,6 +7,9 @@
     using Rocket.Libraries.Validation.Services;
     using System;
 
+    /// <summary>
+    /// Provides a fluent interface for building SQL queries with joins, filters, ordering, and grouping.
+    /// </summary>
     public class QBuilder : IDisposable
     {
         private SelectBuilder _selectBuilder;
@@ -25,12 +28,21 @@
 
         private BuiltQuery builtQuery;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QBuilder"/> class with the parameterization option.
+        /// </summary>
+        /// <param name="parameterize">Determines whether the query should be parameterized for secure parameter binding.</param>
         public QBuilder(bool parameterize)
             : this("t", parameterize)
 
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QBuilder"/> class with the specified alias table name and parameterization option.
+        /// </summary>
+        /// <param name="aliasTablename">The alias for the main table in the query.</param>
+        /// <param name="parameterize">Determines whether the query should be parameterized for secure parameter binding.</param>
         public QBuilder(string aliasTablename, bool parameterize)
             : this(t => t.Name, aliasTablename, parameterize)
 
@@ -83,72 +95,140 @@
 
         private DataValidator DataValidator { get; } = new DataValidator();
 
+        /// <summary>
+        /// Gets the <see cref="SelectBuilder"/> instance for constructing the SELECT clause of the query.
+        /// </summary>
+        /// <returns>The <see cref="SelectBuilder"/> instance.</returns>
         public SelectBuilder UseSelector()
         {
             return _selectBuilder;
         }
 
+
+        /// <summary>
+        /// Gets the <see cref="TableBoundSelectBuilder{TTable}"/> instance for constructing the SELECT clause of the query for a specific table.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to select from.</typeparam>
+        /// <returns>The <see cref="TableBoundSelectBuilder{TTable}"/> instance.</returns>
         public TableBoundSelectBuilder<TTable> UseTableBoundSelector<TTable>()
         {
             return new TableBoundSelectBuilder<TTable>(this, _selectBuilder);
         }
 
+        /// <summary>
+        /// Gets the <see cref="TableBoundGroupBuilder{TTable}"/> instance for constructing the GROUP BY clause of the query for a specific table.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to group by.</typeparam>
+        /// <returns>The <see cref="TableBoundGroupBuilder{TTable}"/> instance.</returns>
         public TableBoundGroupBuilder<TTable> UseTableBoundGrouper<TTable>()
         {
             return new TableBoundGroupBuilder<TTable>(this);
         }
 
+        /// <summary>
+        /// Gets the <see cref="TableBoundJoinBuilder{TLeftTable, TRightTable}"/> instance for constructing a join between two tables.
+        /// </summary>
+        /// <typeparam name="TLeftTable">The type of the left table in the join.</typeparam>
+        /// <typeparam name="TRightTable">The type of the right table in the join.</typeparam>
+        /// <returns>The <see cref="TableBoundJoinBuilder{TLeftTable, TRightTable}"/> instance.</returns>
         public TableBoundJoinBuilder<TLeftTable, TRightTable> UseTableBoundJoinBuilder<TLeftTable, TRightTable>()
         {
             return new TableBoundJoinBuilder<TLeftTable, TRightTable>(this);
         }
 
+        /// <summary>
+        /// Gets the <see cref="TableBoundWhereBuilder{TTable}"/> instance for constructing the WHERE clause of the query for a specific table.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to filter.</typeparam>
+        /// <returns>The <see cref="TableBoundWhereBuilder{TTable}"/> instance.</returns>
         public TableBoundWhereBuilder<TTable> UseTableBoundFilter<TTable>()
         {
             return new TableBoundWhereBuilder<TTable>(_whereBuilder, this);
         }
 
+        /// <summary>
+        /// Gets the <see cref="DerivedTableSelector"/> instance for selecting from a derived table.
+        /// </summary>
+        /// <param name="derivedTable">The <see cref="QBuilder"/> instance representing the derived table.</param>
+        /// <returns>The <see cref="DerivedTableSelector"/> instance.</returns>
         public DerivedTableSelector UseDerivedTableSelector(QBuilder derivedTable)
         {
             return new DerivedTableSelector(derivedTable, _selectBuilder);
         }
 
+        /// <summary>
+        /// Gets the <see cref="IPagingBuilder{TTable}"/> instance for constructing paging logic using SQL Server syntax.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to apply paging to.</typeparam>
+        /// <returns>The <see cref="IPagingBuilder{TTable}"/> instance.</returns>
         public IPagingBuilder<TTable> UseSqlServerPagingBuilder<TTable>()
         {
             return UsePagingBuilder<TTable, SqlServerPagingBuilder<TTable>>(new SqlServerPagingBuilder<TTable>(this));
         }
 
+        /// <summary>
+        /// Gets the <see cref="IPagingBuilder{TTable}"/> instance for constructing paging logic using MySQL Server syntax.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to apply paging to.</typeparam>
+        /// <returns>The <see cref="IPagingBuilder{TTable}"/> instance.</returns>
         public IPagingBuilder<TTable> UseMySqlServerPagingBuilder<TTable>()
         {
             return UsePagingBuilder<TTable, MySqlServerPagingBuilder<TTable>>(new MySqlServerPagingBuilder<TTable>(this));
         }
 
+        /// <summary>
+        /// Gets the <see cref="IPagingBuilder{TTable}"/> instance for constructing paging logic using a custom paging builder.
+        /// </summary>
+        /// <typeparam name="TTable">The type of the table to apply paging to.</typeparam>
+        /// <typeparam name="TBuilderPagingBuilder">The type of the custom paging builder.</typeparam>
+        /// <param name="pagingBuilder">The instance of the custom paging builder.</param>
+        /// <returns>The <see cref="IPagingBuilder{TTable}"/> instance.</returns>
         public IPagingBuilder<TTable> UsePagingBuilder<TTable, TBuilderPagingBuilder>(TBuilderPagingBuilder pagingBuilder)
             where TBuilderPagingBuilder : BuilderBase, IPagingBuilder<TTable>
         {
             return pagingBuilder;
         }
 
+        /// <summary>
+        /// Gets the <see cref="JoinBuilder"/> instance for constructing joins between tables.
+        /// </summary>
+        /// <returns>The <see cref="JoinBuilder"/> instance.</returns>
         public JoinBuilder UseJoiner()
         {
             return _joinBuilder;
         }
 
+        /// <summary>
+        /// Gets the <see cref="WhereBuilder"/> instance for constructing the WHERE clause of the query.
+        /// </summary>
+        /// <returns>The <see cref="WhereBuilder"/> instance.</returns>
         public WhereBuilder UseFilter()
         {
             return _whereBuilder;
         }
 
+        /// <summary>
+        /// Gets the <see cref="OrderBuilder"/> instance for constructing the ORDER BY clause of the query.
+        /// </summary>
+        /// <returns>The <see cref="OrderBuilder"/> instance.</returns>
         public OrderBuilder UseOrdering()
         {
             return _orderBuilder;
         }
 
+        /// <summary>
+        /// Gets the <see cref="GroupBuilder"/> instance for constructing the GROUP BY clause of the query.
+        /// </summary>
+        /// <returns>The <see cref="GroupBuilder"/> instance.</returns>
         public GroupBuilder UseGrouper()
         {
             return _groupBuilder;
         }
 
+        /// <summary>
+        /// Builds the SQL query string based on the configured components (SELECT, JOIN, WHERE, GROUP BY, ORDER BY).
+        /// </summary>
+        /// <returns>The SQL query string.</returns>
         public string Build()
         {
             DataValidator.EvaluateImmediate(string.IsNullOrEmpty(FirstTableName), "There are no tables queued for data querying. Nothing to return");
@@ -164,6 +244,10 @@
             return suffixedQuery.Trim();
         }
 
+        /// <summary>
+        /// Builds the parameterized SQL query string based on the configured components (SELECT, JOIN, WHERE, GROUP BY, ORDER BY).
+        /// </summary>
+        /// <returns>The <see cref="BuiltQuery"/> object containing the parameterized SQL query string.</returns>
         public BuiltQuery BuildWithParameters()
         {
             builtQuery.ParameterizedSql = Build();
